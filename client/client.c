a5cdcec460b46a2512a79bc46aa11b8ce81d7320
Steve Alvarado <33636552+Stevealv121@users.noreply.github.com>
2025-09-16 10:47:59 -0600

    Move client outside of the scope

--- client/client.c
+++ client/client.c
@@ -4,11 +4,12 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-#define SERVER_IP   "158.23.56.208"  
+#define SERVER_IP "158.23.56.208"
 #define SERVER_PORT 1717
 #define BUFFER_SIZE 4096
 
-void send_file(const char *filename) {
+void send_file(const char *filename)
+{
     int sock;
     struct sockaddr_in server_addr;
     char buffer[BUFFER_SIZE];
@@ -17,13 +18,15 @@ void send_file(const char *filename) {
 
     // Abrir archivo
     fp = fopen(filename, "rb");
-    if (!fp) {
+    if (!fp)
+    {
         perror("No se puede abrir el archivo");
         return;
     }
 
     // Crear socket
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
         perror("Error al crear socket");
         fclose(fp);
         return;
@@ -34,7 +37,8 @@ void send_file(const char *filename) {
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(SERVER_PORT);
 
-    if (inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr) <= 0) {
+    if (inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr) <= 0)
+    {
         perror("Dirección IP inválida");
         fclose(fp);
         close(sock);
@@ -42,7 +46,8 @@ void send_file(const char *filename) {
     }
 
     // Conectar
-    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
         perror("Conexión fallida");
         fclose(fp);
         close(sock);
@@ -57,7 +62,8 @@ void send_file(const char *filename) {
 
     // Leer archivo a memoria
     char *filedata = malloc(filesize);
-    if (!filedata) {
+    if (!filedata)
+    {
         perror("Error al reservar memoria");
         fclose(fp);
         close(sock);
@@ -72,15 +78,15 @@ void send_file(const char *filename) {
     // Armar la parte inicial del multipart
     char preamble[BUFFER_SIZE];
     int preamble_len = snprintf(preamble, sizeof(preamble),
-        "--%s\r\n"
-        "Content-Disposition: form-data; name=\"image\"; filename=\"%s\"\r\n"
-        "Content-Type: application/octet-stream\r\n\r\n",
-        boundary, filename);
+                                "--%s\r\n"
+                                "Content-Disposition: form-data; name=\"image\"; filename=\"%s\"\r\n"
+                                "Content-Type: application/octet-stream\r\n\r\n",
+                                boundary, filename);
 
     // Armar la parte final del multipart
     char ending[BUFFER_SIZE];
     int ending_len = snprintf(ending, sizeof(ending),
-        "\r\n--%s--\r\n", boundary);
+                              "\r\n--%s--\r\n", boundary);
 
     // Calcular Content-Length exacto
     long content_length = preamble_len + filesize + ending_len;
@@ -88,12 +94,12 @@ void send_file(const char *filename) {
     // Armar el header HTTP
     char header[BUFFER_SIZE];
     int header_len = snprintf(header, sizeof(header),
-        "POST /upload HTTP/1.1\r\n"
-        "Host: %s:%d\r\n"
-        "Content-Type: multipart/form-data; boundary=%s\r\n"
-        "Content-Length: %ld\r\n"
-        "Connection: close\r\n\r\n",
-        SERVER_IP, SERVER_PORT, boundary, content_length);
+                              "POST /upload HTTP/1.1\r\n"
+                              "Host: %s:%d\r\n"
+                              "Content-Type: multipart/form-data; boundary=%s\r\n"
+                              "Content-Length: %ld\r\n"
+                              "Connection: close\r\n\r\n",
+                              SERVER_IP, SERVER_PORT, boundary, content_length);
 
     // Enviar cabecera HTTP
     send(sock, header, header_len, 0);
@@ -111,7 +117,8 @@ void send_file(const char *filename) {
     printf("Imagen '%s' enviada (%ld bytes)\n", filename, filesize);
 
     // Leer respuesta del servidor
-    while ((bytes = recv(sock, buffer, sizeof(buffer)-1, 0)) > 0) {
+    while ((bytes = recv(sock, buffer, sizeof(buffer) - 1, 0)) > 0)
+    {
         buffer[bytes] = '\0';
         printf("%s", buffer);
     }
@@ -120,9 +127,11 @@ void send_file(const char *filename) {
     close(sock);
 }
 
-int main() {
+int main()
+{
     char filename[256];
-    while (1) {
+    while (1)
+    {
         printf("\nIngrese el nombre de la imagen (o Exit para salir): ");
         scanf("%s", filename);
         if (strcmp(filename, "Exit") == 0)
